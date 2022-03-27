f7ab4518189c4255941681ca7fe73143efe61e08
Messichikito2006 <102479141+Messichikito2006@users.noreply.github.com>
2022-03-27 19:29:04 -0300

    Update Hetero.js

--- plugins/Hetero.js
+++ plugins/Hetero.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que desee calcular su porcentaje de ${command.replace('how', '')}*`
 conn.reply(m.chat, `
-_*${text}* *es 🏴🏳️🏴* *${Math.floor(Math.random() * 0)}%* *${command.replace('how', '').toUpperCase()}*_
+_*${text}* *es 🏴🏳️🏴* *${Math.floor(Math.random() * 100)}%* *${command.replace('how', '').toUpperCase()}*_
 `.trim(), m, m.mentionedJid ? {
 contextInfo: {
 mentionedJid: m.mentionedJid}} : {})}
