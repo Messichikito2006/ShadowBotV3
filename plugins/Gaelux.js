0076309bec3d1ca8ee53e55045b2b71c134a293a
Messichikito2006 <102479141+Messichikito2006@users.noreply.github.com>
2022-03-27 22:11:02 -0300

    Update Gaelux.js

--- plugins/Gaelux.js
+++ plugins/Gaelux.js
@@ -1,6 +1,25 @@
 let handler = async m => m.reply(`
 
-Suscfjcfgv
+
+♦️𝕯𝖎𝖘𝖈𝖔𝖗𝖉 : https://discord.gg/9pBZuyn8kF
+
+♦️𝕿𝖜𝖎𝖙𝖈𝖍 : https://www.twitch.tv/thegaelux?sr=a
+
+♦️𝕸𝖎 𝖙𝖎𝖐𝕿𝖔𝖐 𝖔𝖋𝖎𝖈𝖎𝖆𝖑 : https://vm.tiktok.com/ZM8edRgTR/
+
+🎀𝕸𝖎 𝕴𝖓𝖘𝖙𝖆𝖌𝖗𝖆𝖒:  https://instagram.com/soygaelux?utm_medium=copy_link
+
+⭕𝘾𝙖𝙣𝙖𝙡𝙚𝙨:
+        
+ 🔸𝕋𝕙𝕖𝔾𝕒𝕖𝕝𝕦𝕩: https://youtube.com/c/TheGaelux
+
+ 🔸𝔾𝕒𝕖𝕝𝕦𝕩 𝕊𝕚𝕞𝕡: https://youtube.com/channel/UC4gIxXOLOnDiTWjvk6oN8rw
+
+ 🔸𝔾𝕒𝕖𝕝𝔹𝕠𝕩: https://youtube.com/channel/UCxlbjGVi6knKNcgVwNeoeXg
+
+
+Gracias por su apoyo!! :D
+
 
 `.trim()) 
 handler.help = ['gaelux']
